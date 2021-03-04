76a1064c15c8426abdc05f602c5fadaaf6aea4c2
Alexandre Santos <alexandre.santozz@gmail.com>
2021-03-04 00:58:46 +0000

    fix(chapter08): use correct chapter names in deps

--- Chapter08/sections/7-final-tested-version/museums-api/src/deps.ts
+++ Chapter08/sections/7-final-tested-version/museums-api/src/deps.ts
@@ -2,10 +2,10 @@ export { serve } from "https://deno.land/std@0.72.0/http/server.ts";
 export { Application, Router } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 export type {
   Algorithm,
-} from "https://raw.githubusercontent.com/PacktPublishing/Deno-Web-Development/master/ch6/jwt-auth/mod.ts";
+} from "https://raw.githubusercontent.com/PacktPublishing/Deno-Web-Development/master/Chapter06/jwt-auth/mod.ts";
 export {
   Repository as AuthRepository,
-} from "https://raw.githubusercontent.com/PacktPublishing/Deno-Web-Development/master/ch6/jwt-auth/mod.ts";
+} from "https://raw.githubusercontent.com/PacktPublishing/Deno-Web-Development/master/Chapter06/jwt-auth/mod.ts";
 export type {
   OnFailureHandler,
   OnSuccessHandler,
