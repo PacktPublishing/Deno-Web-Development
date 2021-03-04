af54ca653a4ccf2a3e7bcdf99e27ba128c478e63
Alexandre Santos <alexandre.santozz@gmail.com>
2021-03-04 13:11:28 +0000

    fix(chapter02): added final version of list files

---
+++ Chapter03/ls/list-file-names-modification-date.ts
@@ -0,0 +1,18 @@
+import { join } from 'https://deno.land/std@0.89.0/path/mod.ts';
+const [path = "."] = Deno.args;
+
+for await (const dir of Deno.readDir(path)) {
+  let fileInfo = await Deno.stat(join(path,dir.name));
+
+  const modificationTime = fileInfo.mtime;
+  const message = [
+    fileInfo.size.toString().padEnd(4),
+
+    `${modificationTime?.getUTCMonth().toString().padStart(2)}/${
+      modificationTime?.getUTCDay().toString().padEnd(2)
+    }`,
+    dir.name,
+  ];
+
+  console.log(message.join(""));
+}
