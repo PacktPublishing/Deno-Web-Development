118342ae8f063022a26ae029d573a8fb65b9e76b
Alexandre Santos <alexandre.santozz@gmail.com>
2021-03-22 17:58:24 +0000

    fix(chapter02): prefinals review

--- Chapter02/hello-http-server.js
+++ Chapter02/hello-http-server.js
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.64.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.83.0/http/server.ts";
 for await (const req of serve(":8080")) {
   req.respond({ body: "Hello deno" });
 }
